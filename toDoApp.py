2790b52bd37e715d2502cf60b317b3691ae30654
SEAN LOPEZ <seanpatty36@gmail.com>
2025-09-22 15:47:57 +0800

    Clean up formatting and change function name to snake_case convention

--- toDoApp.py
+++ toDoApp.py
@@ -2,18 +2,18 @@
 
 tasks = []
 
-def addtask(task):
+def add_task(task):
     tasks.append(task)
     print("task added!")
 
-def showTasks():
+def show_tasks():
     if len(tasks) == 0:
         print("no tasks yet")
     else:
         for i in range(len(tasks)):
             print(i + 1, ".", tasks[i])
 
-def removetask(tasknumber):
+def remove_task(tasknumber):
     tasks.pop(tasknumber)
     print("task removed!!")
 
@@ -22,20 +22,21 @@ def main():
         print("1 Add Task")
         print("2.Show Tasks")
         print("3.Remove Task")
-        print("4- Exit")
-        ch = input("enter choice : ")
+        print("4. Exit")
+        ch = input("Enter choice : ")
         
         if ch == "1":
-            t = input("enter task : ")
+            t = input("Enter task : ")
             addtask(t)
         elif ch == "2":
             showTasks()
         elif ch == "3":
-            n = int(input("enter task no to remove: "))
+            n = int(input("Enter task number to remove: "))
             removetask(n)
         elif ch == "4":
             break
         else:
-            print("wrong choice!!")
+            print("Wrong choice!")
 
-main()
+if __name__ == "__main__":
+    main()
