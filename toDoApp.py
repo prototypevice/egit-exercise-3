6e33d628e3a506525342e63ac1dae4676a1e029f
SEAN LOPEZ <seanpatty36@gmail.com>
2025-09-22 15:56:13 +0800

    Fixed remove_tasks error and added error handling on inputs

--- toDoApp.py
+++ toDoApp.py
@@ -3,37 +3,46 @@
 tasks = []
 
 def add_task(task):
+    if not task or not task.strip():
+        print("You didn't input any tasks.\n")
+        return
     tasks.append(task)
-    print("task added!")
+    print("Task added!\n")
 
 def show_tasks():
     if len(tasks) == 0:
-        print("no tasks yet")
+        print("No tasks yet.")
     else:
+        print("Tasks list.")
         for i in range(len(tasks)):
             print(i + 1, ".", tasks[i])
 
-def remove_task(tasknumber):
-    tasks.pop(tasknumber)
-    print("task removed!!")
+def remove_task(task_number):
+    index = task_number - 1
+    if index < 0 or index >= len(tasks):
+        print("Invalid task number.")
+        return
+    tasks.pop(index)
+    print("Task removed!")
 
 def main():
     while True:
-        print("1 Add Task")
-        print("2.Show Tasks")
-        print("3.Remove Task")
+        print("1. Add Tasks")
+        print("2. Show Tasks")
+        print("3. Remove Tasks")
         print("4. Exit")
-        ch = input("Enter choice : ")
+        ch = input("Enter choice: ")
         
         if ch == "1":
-            t = input("Enter task : ")
-            addtask(t)
+            t = input("Enter task: ")
+            add_task(t)
         elif ch == "2":
-            showTasks()
+            show_tasks()
         elif ch == "3":
             n = int(input("Enter task number to remove: "))
-            removetask(n)
+            remove_task(n)
         elif ch == "4":
+            print("Thank you for using our program!")
             break
         else:
             print("Wrong choice!")
