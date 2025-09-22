3d1142eaffb037aa9bfb7160f9f4493af2843b2a
SEAN LOPEZ <seanpatty36@gmail.com>
2025-09-22 16:00:02 +0800

    Added lines and new spaces to make the output of the code inside the terminal much more cleaner to read

--- toDoApp.py
+++ toDoApp.py
@@ -11,26 +11,31 @@ def add_task(task):
 
 def show_tasks():
     if len(tasks) == 0:
-        print("No tasks yet.")
+        print("No tasks yet.\n")
     else:
-        print("Tasks list.")
+        print("Tasks list:")
         for i in range(len(tasks)):
             print(i + 1, ".", tasks[i])
+        
+        print("\n")
 
 def remove_task(task_number):
     index = task_number - 1
     if index < 0 or index >= len(tasks):
-        print("Invalid task number.")
+        print("Invalid task number.\n")
         return
     tasks.pop(index)
-    print("Task removed!")
+    print("Task removed!\n")
 
 def main():
     while True:
+        print("To Do App in Python")
+        print("===============")
         print("1. Add Tasks")
         print("2. Show Tasks")
         print("3. Remove Tasks")
         print("4. Exit")
+        print("===============")
         ch = input("Enter choice: ")
         
         if ch == "1":
@@ -38,9 +43,15 @@ def main():
             add_task(t)
         elif ch == "2":
             show_tasks()
+
         elif ch == "3":
-            n = int(input("Enter task number to remove: "))
-            remove_task(n)
+            try:
+                n = int(input("Enter task number to remove: "))
+                remove_task(n)
+
+            except ValueError:
+                print("Please enter a valid number.")
+
         elif ch == "4":
             print("Thank you for using our program!")
             break
