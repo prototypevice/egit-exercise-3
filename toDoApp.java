39ee2ba1b25c4c7184508fcefd7e8a0ac6b59655
GonSci <igonneski@gmail.com>
2025-09-22 15:27:03 +0800

    fix: add task, show task, remove task logic and formatted output properly

--- toDoApp.java
+++ toDoApp.java
@@ -1,8 +1,8 @@
 // ToDoApp.java
 import java.util.*;
 
-public class ToDoApp {
-    static ArrayList tasks = new ArrayList();
+public class toDoApp {
+    static ArrayList<String> tasks = new ArrayList<String>();
 
     public static void addtask(String t) {
         tasks.add(t);
@@ -10,43 +10,61 @@ public class ToDoApp {
     }
 
     public static void showTasks() {
-        if (tasks.size() == 0) {
+        if (tasks.isEmpty()) {
             System.out.println("No tasks yet");
         } else {
-            for (int i = 0; i <= tasks.size(); i++) {
+            for (int i = 0; i < tasks.size(); i++) {
                 System.out.println((i + 1) + ". " + tasks.get(i));
             }
         }
     }
 
     public static void removeTask(int n) {
-        tasks.remove(n);
-        System.out.println("task removed");
+        if (n > 0 && n <= tasks.size()) {
+            tasks.remove(n - 1); 
+            System.out.println("Task removed!");
+        } else {
+            System.out.println("Invalid task number!");
+        }
     }
 
     public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
         while (true) {
+            System.out.println("--------------------------"); // para malupit ung format man
             System.out.println("1 Add Task"); 
             System.out.println("2.Show Tasks");
             System.out.println("3.Remove Task");
             System.out.println("4 Exit");
+            System.out.println("--------------------------");
+            System.out.print("Enter Choice: ");
+
             int choice = s.nextInt(); 
 
-            if (choice == 1) {
-                System.out.println("Enter Task: ");
-                String t = s.nextLine();
-                addtask(t);
-            } else if (choice == 2) {
-                showTasks();
-            } else if (choice == 3) {
-                System.out.print("Enter task no to remove: ");
-                int n = s.nextInt();
-                removeTask(n); 
-            } else if (choice == 4) {
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter Task: ");
+                    s.nextLine();
+                    String t = s.nextLine();
+                    addtask(t);
+                    break;
+                case 2:
+                    showTasks();
+                    break;
+                case 3:
+                    System.out.print("Enter task no to remove: ");
+                    int n = s.nextInt();
+                    removeTask(n); 
+                    break;
+                case 4:
+                    s.close();
+                    break;
+                default:
+                    System.out.print("Wrong Choice!!");
+                    break;
+            }
+            if (choice == 4) {
                 break;
-            } else {
-                System.out.println("Wrong Choice!!");
             }
         }
     }
